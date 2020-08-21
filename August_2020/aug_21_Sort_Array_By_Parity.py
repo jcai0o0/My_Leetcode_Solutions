2bedbfb2b943b935993c506cfc9e79429dd01be6
Janet <44845593+jcai0o0@users.noreply.github.com>
2020-08-21 15:24:39 -0400

    Leetcode August code challenge

---
+++ August_2020/aug_21_Sort_Array_By_Parity.py
@@ -0,0 +1,19 @@
+# rainbow sort
+# all the elements before curr (exclude) are even numbers
+# all the elements between curr (include) and partition (include) are waiting to be explore
+# all the elements after partition are odd numbers
+# when curr and partition meet each other, the array is all set
+# Time complexity : O(n)
+# Space complexity : O(1)
+
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        curr = 0
+        partition = len(A)-1
+        while curr < partition:
+            if A[curr] % 2 == 1:
+                A[curr], A[partition] = A[partition], A[curr]
+                partition -= 1
+            elif A[curr] % 2 == 0:
+                curr += 1
+        return A
