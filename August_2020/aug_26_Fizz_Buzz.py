1e0b337c073e440b6c89918a5b42e902bb2d0c01
Janet <44845593+jcai0o0@users.noreply.github.com>
2020-08-26 21:59:00 -0400

    Leetcode August code challenge

---
+++ August_2020/aug_26_Fizz_Buzz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        i = 1
+        res = []
+        while i <= n:
+            if i%3 == 0 and i%5 == 0:
+                res.append('FizzBuzz')
+            elif i%3 == 0:
+                res.append('Fizz')
+            elif i%5 == 0:
+                res.append('Buzz')
+            else:
+                res.append(str(i))
+            i += 1
+        return res
